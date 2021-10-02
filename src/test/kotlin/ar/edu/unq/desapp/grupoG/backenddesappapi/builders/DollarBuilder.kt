ef89922d454eefba92d73deba476a12cfa49be76
EnzoGalarza <EnzoGl33@hotmail.com>
2021-10-02 20:55:36 -0300

    Add dollar builder

---
+++ src/test/kotlin/ar/edu/unq/desapp/grupoG/backenddesappapi/builders/DollarBuilder.kt
@@ -0,0 +1,15 @@
+package ar.edu.unq.desapp.grupoG.backenddesappapi.builders
+
+import ar.edu.unq.desapp.grupoG.backenddesappapi.model.DollarQuoteDTO
+
+class DollarBuilder(
+    var d : String = "2/10/2021",
+    var v : Double = 185.0
+) {
+    fun build() = DollarQuoteDTO(d,v)
+
+    fun withD(d:String) : DollarBuilder{
+        this.d = d
+        return this
+    }
+}
