d92b7400ba3b22083cab94fe4462f4686fbfb266
EnzoGalarza <EnzoGl33@hotmail.com>
2021-10-05 22:22:56 -0300

    Fix mocking repository in DollarService tests

--- src/test/kotlin/ar/edu/unq/desapp/grupoG/backenddesappapi/DollarServiceTest.kt
+++ src/test/kotlin/ar/edu/unq/desapp/grupoG/backenddesappapi/DollarServiceTest.kt
@@ -24,7 +24,7 @@ class DollarServiceTest {
     lateinit var restTemplate : RestTemplate
 
     @InjectMocks
-    var dollarService = DollarQuoteService()
+    lateinit var dollarService : DollarQuoteService
 
     @Before
     fun setUp(){
