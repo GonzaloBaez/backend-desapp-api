d1d99ca922202a8b2c6dae858a180ac4f0cf3159
EnzoGalarza <EnzoGl33@hotmail.com>
2021-10-05 22:16:56 -0300

    Mocking UserService

--- src/test/kotlin/ar/edu/unq/desapp/grupoG/backenddesappapi/UserServiceTests.kt
+++ src/test/kotlin/ar/edu/unq/desapp/grupoG/backenddesappapi/UserServiceTests.kt
@@ -3,26 +3,33 @@ package ar.edu.unq.desapp.grupoG.backenddesappapi
 import ar.edu.unq.desapp.grupoG.backenddesappapi.exceptions.NotFoundException
 import ar.edu.unq.desapp.grupoG.backenddesappapi.model.User
 import ar.edu.unq.desapp.grupoG.backenddesappapi.builders.UserBuilder
+import ar.edu.unq.desapp.grupoG.backenddesappapi.repositories.UserRepository
 import ar.edu.unq.desapp.grupoG.backenddesappapi.services.UserService
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.After
 import org.junit.Before
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 import org.springframework.dao.DataIntegrityViolationException
+import java.util.*
 
-@RunWith(SpringRunner::class)
+@RunWith(MockitoJUnitRunner::class)
 @SpringBootTest
 class UserServiceTests {
 
 	private lateinit var userBuilder : UserBuilder
 
-	@Autowired
-	lateinit var userService: UserService
+	@Mock
+	lateinit var userRepository: UserRepository
+
+	@InjectMocks
+	lateinit var userService : UserService
 
 	@Before
 	fun setUp(){
@@ -30,41 +37,81 @@ class UserServiceTests {
 	}
 
 	@Test
-	fun savingAUserAndGettingIt() {
+	fun savingAUserAndGettingItById() {
 		var userToRegister:User = userBuilder.build()
-		userService.save(userToRegister)
-		var registeredUser:User= userService.findById(userToRegister.id)
 
-		assertThat(userToRegister == registeredUser)
+		Mockito.`when`(userRepository.save(userToRegister)).thenReturn(userToRegister)
+		Mockito.`when`(userRepository.findById(userToRegister.id)).thenReturn(Optional.of(userToRegister))
+
+		val userRegistered = userService.save(userToRegister)
+		var obtainedUser :User= userService.findById(userToRegister.id)
+
+		assertThat(userRegistered == obtainedUser)
 	}
 
 	@Test
-	fun gettingShouldFailIfIdDoesnotExist(){
+	fun savingAUserAndGettingItByEmail(){
+		var userToRegister = userBuilder.build()
+
+		Mockito.`when`(userRepository.save(userToRegister)).thenReturn(userToRegister)
+		Mockito.`when`(userRepository.findByEmail(userToRegister.email)).thenReturn(Optional.of(userToRegister))
+
+		val userRegistered = userService.save(userToRegister)
+		val savedUser = userService.findByEmail(userToRegister.email)
+
+		assertThat(userRegistered == savedUser)
+	}
+
+	@Test
+	fun `gettingShouldFailIfIdDoesn'tExist`(){
 		assertThatThrownBy { userService.findById(100) }.isInstanceOf(NotFoundException::class.java)
 			.withFailMessage("User with id 100 doesn't exist")
 	}
 
 	@Test
-	fun savingShouldFailIfTwoUsersHasSameWallet(){
-		var userOne : User = userBuilder.withWallet("12345678").withEmail("asd@asd").build()
-		var userTwo : User = userBuilder.withWallet("12345678").withEmail("asd2@asd").build()
+	fun `gettingShouldFailIfEmailDoesn'tExist`(){
+		assertThatThrownBy { userService.findByEmail("100@gmail.com") }.isInstanceOf(NotFoundException::class.java)
+			.withFailMessage("User with email 100@gmail.com doesn't exist")
+	}
 
+	@Test
+	fun savingShouldFailIfTwoUsersHasSameWallet(){
+		var userOne : User = userBuilder.withWallet("12345678").build()
+		var userTwo : User = userBuilder.withWallet("12345678").build()
+
+		Mockito.`when`(userRepository.save(userOne)).thenReturn(userOne)
 		userService.save(userOne)
+		Mockito.`when`(userRepository.save(userTwo)).thenThrow(DataIntegrityViolationException::class.java)
 
 		assertThatThrownBy { userService.save(userTwo) }.isInstanceOf(DataIntegrityViolationException::class.java)
+	}
 
+	@Test
+	fun savingShouldFailIfTwoUsersHasSameEmail(){
+		var userOne : User = userBuilder.withEmail("userOne@gmail.com").build()
+		var userTwo : User = userBuilder.withEmail("userOne@gmail.com").build()
+
+		Mockito.`when`(userRepository.save(userOne)).thenReturn(userOne)
+		userService.save(userOne)
+		Mockito.`when`(userRepository.save(userTwo)).thenThrow(DataIntegrityViolationException::class.java)
+
+		assertThatThrownBy { userService.save(userTwo) }.isInstanceOf(DataIntegrityViolationException::class.java)
 	}
 
 	@Test
 	fun gettingAllUsers(){
-		var userOne = userBuilder.withWallet("12345678").withEmail("asd@asd").build()
-		var userTwo = userBuilder.withWallet("04040400").withEmail("asd2@asd").build()
-		userService.save(userOne)
-		userService.save(userTwo)
+		var userOne = userBuilder.withWallet("12345678").withEmail("userOne@asd").build()
+		var userTwo = userBuilder.withWallet("04040400").withEmail("userTwo@asd").build()
 
-		var allUsers = userService.findAll().toMutableList()
+		Mockito.`when`(userRepository.save(userOne)).thenReturn(userOne)
+		Mockito.`when`(userRepository.save(userTwo)).thenReturn(userTwo)
+		Mockito.`when`(userRepository.findAll()).thenReturn(listOf(userOne,userTwo))
 
-		assertThat(allUsers.size == 2)
+		var listOfUser = listOf(userService.save(userOne),userService.save(userTwo))
+		var allSavedUsers = userService.findAll().toMutableList()
+
+		assertThat(allSavedUsers.size == 2)
+		assertThat(listOfUser == allSavedUsers)
 	}
 
 	@After
