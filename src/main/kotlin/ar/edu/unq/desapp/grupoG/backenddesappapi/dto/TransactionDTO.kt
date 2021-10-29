8c2b4b17e1fa78c0d65c7425ef998093116768b1
EnzoGalarza <EnzoGl33@hotmail.com>
2021-10-28 22:13:03 -0300

    Add transaction DTO

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoG/backenddesappapi/dto/TransactionDTO.kt
@@ -0,0 +1,21 @@
+package ar.edu.unq.desapp.grupoG.backenddesappapi.dto
+
+import ar.edu.unq.desapp.grupoG.backenddesappapi.model.Transaction
+import ar.edu.unq.desapp.grupoG.backenddesappapi.model.User
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+data class TransactionDTO(
+    var user : String,
+    val hour : String = LocalDateTime.now().toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm")),
+    val cryptoName : String,
+    var unitValue : Double,
+    var quote : Double,
+    var totalPrice : Double,
+    var amount : Double
+) {
+
+    fun toModel(user: User) : Transaction{
+        return Transaction(user,hour,cryptoName,unitValue,quote,totalPrice,amount)
+    }
+}
