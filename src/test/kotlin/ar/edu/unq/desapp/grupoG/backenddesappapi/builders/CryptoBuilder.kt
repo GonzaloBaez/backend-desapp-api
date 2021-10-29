16317aee4f570245a242bd3bf2573d2a8328f622
EnzoGalarza <EnzoGl33@hotmail.com>
2021-10-28 21:59:07 -0300

    Change symbol in crypto builder

--- src/test/kotlin/ar/edu/unq/desapp/grupoG/backenddesappapi/builders/CryptoBuilder.kt
+++ src/test/kotlin/ar/edu/unq/desapp/grupoG/backenddesappapi/builders/CryptoBuilder.kt
@@ -3,7 +3,7 @@ package ar.edu.unq.desapp.grupoG.backenddesappapi.builders
 import ar.edu.unq.desapp.grupoG.backenddesappapi.model.CryptoDTO
 
 class CryptoBuilder(
-    var symbol : String = "BNDUSDT",
+    var symbol : String = "BNBUSDT",
     var price : Double = 375.0
 ) {
 
