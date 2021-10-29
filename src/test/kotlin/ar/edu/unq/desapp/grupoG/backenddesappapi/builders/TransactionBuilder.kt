7e0396d4745f9c4df9657975888dafc72676e4d9
EnzoGalarza <EnzoGl33@hotmail.com>
2021-10-28 21:57:59 -0300

    Add transaction builder

---
+++ src/test/kotlin/ar/edu/unq/desapp/grupoG/backenddesappapi/builders/TransactionBuilder.kt
@@ -0,0 +1,21 @@
+package ar.edu.unq.desapp.grupoG.backenddesappapi.builders
+
+import ar.edu.unq.desapp.grupoG.backenddesappapi.model.Transaction
+import ar.edu.unq.desapp.grupoG.backenddesappapi.model.User
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class TransactionBuilder(
+    var user: User = UserBuilder().build(),
+    val hour : String = LocalDateTime.now().toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm")),
+    val cryptoName : String = "BTC", var unitValue : Double = 135.0, var quote : Double = 24975.0,
+    var totalPrice : Double = 49950.0, var amount : Double = 2.0
+) {
+    fun build() = Transaction(user,hour,cryptoName,unitValue,quote,totalPrice,amount)
+
+    fun withUser(user : User) : TransactionBuilder{
+        this.user = user
+        return this
+    }
+}
