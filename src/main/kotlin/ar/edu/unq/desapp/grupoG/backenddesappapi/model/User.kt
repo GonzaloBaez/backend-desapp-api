7b1db5aec37caf542431d5b4d22c555915aeb16c
EnzoGalarza <EnzoGl33@hotmail.com>
2021-10-28 22:07:40 -0300

    Add transactions to user

--- src/main/kotlin/ar/edu/unq/desapp/grupoG/backenddesappapi/model/User.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoG/backenddesappapi/model/User.kt
@@ -1,5 +1,7 @@
 package ar.edu.unq.desapp.grupoG.backenddesappapi.model
 
+import ar.edu.unq.desapp.grupoG.backenddesappapi.dto.UserDTO
+import com.fasterxml.jackson.annotation.JsonIgnore
 import javax.persistence.*
 
 @Entity
@@ -7,11 +9,32 @@ import javax.persistence.*
 data class User(@Column(length = 30) var name : String, @Column(length = 30) var surname : String,
                 @Column(unique = true) var email : String, @Column(length = 30) var address : String,
                 @Column var password : String, @Column(length = 22) var cvu : String,
-                @Column(length = 8,unique = true) var wallet : String ) {
+                @Column(length = 8,unique = true) var wallet : String,
+                @Column var points : Int = 0, @Column var operations : Int = 0) {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id : Long = 0
+
+    @OneToMany(mappedBy = "user",cascade = arrayOf(CascadeType.ALL))
+    @JsonIgnore
+    var transactions : MutableList<Transaction> = mutableListOf()
+
+    fun newOperation() {
+        operations++
+    }
+
+    fun addTransaction(tr : Transaction){
+        this.transactions.add(tr)
+        newOperation()
+    }
+
+    fun toDTO() : UserDTO {
+        lateinit var reputation : String
+        reputation = if(operations == 0) "Sin operaciones" else (this.points / this.operations).toString()
+
+        return UserDTO(name,surname,email,address,cvu,wallet,points,operations,reputation)
+    }
 
 
 }
