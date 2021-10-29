55a01accaa6ad9de54472ec2c9c2555c9cc4e921
EnzoGalarza <EnzoGl33@hotmail.com>
2021-10-28 22:05:53 -0300

    Add user model test

---
+++ src/test/kotlin/ar/edu/unq/desapp/grupoG/backenddesappapi/model/UserModelTest.kt
@@ -0,0 +1,43 @@
+package ar.edu.unq.desapp.grupoG.backenddesappapi.model
+
+
+import ar.edu.unq.desapp.grupoG.backenddesappapi.builders.UserBuilder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class UserModelTest {
+    private lateinit var userBuilder : UserBuilder
+
+    @Mock
+    private lateinit var transaction : Transaction
+
+    @Before
+    fun setUp(){
+        userBuilder = UserBuilder()
+    }
+
+    @Test
+    fun aNewUserHasZeroOperationsAndZeroPoints(){
+        var newUser = userBuilder.build()
+
+        assertEquals(newUser.operations,0)
+        assertEquals(newUser.points,0)
+    }
+
+    @Test
+    fun whenANewUserAddATransactionHeHasOneOperation(){
+        var newUser = userBuilder.build()
+
+        newUser.addTransaction(transaction)
+
+        assertEquals(newUser.operations,1)
+        assertEquals(newUser.transactions.size,1)
+        assertTrue(newUser.transactions.contains(transaction))
+    }
+}
