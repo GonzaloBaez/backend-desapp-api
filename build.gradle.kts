6dae2ed431d1d5216125635f799f3025ff4a2460
EnzoGalarza <EnzoGl33@hotmail.com>
2021-11-29 17:26:20 -0300

    Agrego excepcion en sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ sonarqube {
 		property ("sonar.login","795ec1c68f8a2a0cb50f4cd4ce1082b3d4e6ccba")
 		property ("sonar.coverage.jacoco.xmlReportPaths", "build/jacocoXML")
 		property ("sonar.exclusions","**/securityConfig/*, **/dto/*, **/cache/*, **/swagger/*, **/aspects/*, " +
-				"**/JwtAuthenticationController.kt, **/JwtUserDetailsService.kt, **/GlobalExceptionHandler.kt")
+				"**/JwtAuthenticationController.kt, **/JwtUserDetailsService.kt, **/GlobalExceptionHandler.kt, **/InitServiceInMemory.kt")
 	}
 }
 jacoco {
